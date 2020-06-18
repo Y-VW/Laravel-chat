aa9d8c0458d7e3c75620c34f2c86d95912291216
Yvana Wagter <yvana@wagter.nl>
2020-06-18 16:59:36 +0200

    build chat with pusher/laravel/vue

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+use Illuminate\Support\Facades\Auth;
+use App\Events\MessageSent;
+use App\User;
+
+class ChatsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show chats
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+
+     public function index()
+     {
+        return view('chat');
+     }
+
+     /**
+      * Fetch all messages
+      * @return Message
+      */
+    public function fetchMessages()
+    {
+        return Message::with('user')->get();
+    }
+    /**
+     * Persist message to database
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function sendMessage(Request $request)
+    {
+        $user = Auth::user();
+        
+        $message = User::findOrFail($user->id)->messages()->create([
+            'message' => $request->input('message')
+        ]);
+        broadcast(new MessageSent($user, $message))->toOthers();
+
+        return ['status' => 'Message sent!'];
+    }
+}
